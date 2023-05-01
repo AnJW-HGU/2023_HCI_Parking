b8f62f86cc71d80f88f2e305fa73503f2069d8dc
AnJW-HGU <22000404@handong.ac.kr>
2023-05-01 21:50:51 +0900

    feat: Add fonts and make test style

---
+++ lib/util/test_style.dart
@@ -0,0 +1,67 @@
+import 'dart:ui';
+
+import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+
+TextStyle pretendardBlod14 = TextStyle(
+  fontFamily: 'PretendardVariable',
+  fontVariations: const <FontVariation>[FontVariation('wght', 700.0)],
+  fontSize: 14.0.sp,
+  letterSpacing: 0,
+);
+
+TextStyle pretendardSemiBlod13 = TextStyle(
+  fontFamily: 'PretendardVariable',
+  fontVariations: const <FontVariation>[FontVariation('wght', 600.0)],
+  fontSize: 13.0.sp,
+  letterSpacing: 0,
+);
+
+TextStyle pretendardMedium18 = TextStyle(
+  fontFamily: 'PretendardVariable',
+  fontVariations: const <FontVariation>[FontVariation('wght', 500.0)],
+  fontSize: 18.0.sp,
+  letterSpacing: 0,
+);
+
+TextStyle pretendardMedium14 = TextStyle(
+  fontFamily: 'PretendardVariable',
+  fontVariations: const <FontVariation>[FontVariation('wght', 500.0)],
+  fontSize: 14.0.sp,
+  letterSpacing: 0,
+);
+
+TextStyle pretendardMedium13 = TextStyle(
+  fontFamily: 'PretendardVariable',
+  fontVariations: const <FontVariation>[FontVariation('wght', 500.0)],
+  fontSize: 13.0.sp,
+  letterSpacing: 0,
+);
+
+TextStyle pretendardMedium11 = TextStyle(
+  fontFamily: 'PretendardVariable',
+  fontVariations: const <FontVariation>[FontVariation('wght', 500.0)],
+  fontSize: 11.0.sp,
+  letterSpacing: 0,
+);
+
+TextStyle pretendardRegular20 = TextStyle(
+  fontFamily: 'PretendardVariable',
+  fontVariations: const <FontVariation>[FontVariation('wght', 400.0)],
+  fontSize: 20.0.sp,
+  letterSpacing: 0,
+);
+
+TextStyle pretendardRegular13 = TextStyle(
+  fontFamily: 'PretendardVariable',
+  fontVariations: const <FontVariation>[FontVariation('wght', 400.0)],
+  fontSize: 13.0.sp,
+  letterSpacing: 0,
+);
+
+TextStyle pretendardLight20 = TextStyle(
+  fontFamily: 'PretendardVariable',
+  fontVariations: const <FontVariation>[FontVariation('wght', 300.0)],
+  fontSize: 20.0.sp,
+  letterSpacing: 0,
+);
