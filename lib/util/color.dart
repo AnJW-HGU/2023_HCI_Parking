086f0013c5991e41108b8405e4c557e5359bf40a
AnJW-HGU <22000404@handong.ac.kr>
2023-05-01 12:21:45 +0900

    feat: Add colors file

---
+++ lib/util/color.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+const whiteColor = Color(0xffFFFFFF);
+const blackColor = Color(0xff262626);
+
+const primaryColor = Color(0xff235597);
+const secondaryColor = Color(0xffE1A939);
+
+const iconColor1 = Color(0xff8B8B8B);
+const iconColor2 = Color(0xff143459);
+
+const appBarColor = Color(0xff3E454D);
+
+const greyColor1 = Color(0xffEEEEEE);
+const greyColor2 = Color(0xff4E4D4D);
