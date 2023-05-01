5e2282f4fb2ef0cee44470733fcc9523027c083c
AnJW-HGU <22000404@handong.ac.kr>
2023-05-01 12:23:22 +0900

    feat: Add colorful safe area in splash screen

--- lib/screen/splash_screen.dart
+++ lib/screen/splash_screen.dart
@@ -1,26 +1,31 @@
+import 'package:colorful_safe_area/colorful_safe_area.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:flutter_svg/flutter_svg.dart';
+import 'package:hci_parking/util/color.dart';
 
 class SplashScreen extends StatelessWidget {
   const SplashScreen({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      body: Center(
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.center,
-          children: [
-            SizedBox(
-              height: 196.h,
-            ),
-            Image.asset(
-              'asset/logo_handong.png',
-              width: 193,
-              height: 190,
-            ),
-          ],
+    return ColorfulSafeArea(
+      color: whiteColor,
+      child: Scaffold(
+        body: Center(
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: [
+              SizedBox(
+                height: 196.h,
+              ),
+              Image.asset(
+                'asset/logo_handong.png',
+                width: 193,
+                height: 190,
+              ),
+            ],
+          ),
         ),
       ),
     );
