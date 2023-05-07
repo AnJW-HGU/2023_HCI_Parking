16a0195bdc6636beed2323823fdb85f873148d79
AnJW-HGU <22000404@handong.ac.kr>
2023-05-08 03:15:56 +0900

    feat: Set firebase core

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,13 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:get/get.dart';
 import 'package:hci_parking/screen/home_screen.dart';
 import 'package:hci_parking/screen/splash_screen.dart';
 
-void main() {
+Future<void> main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp();
   runApp(const MyApp());
 }
 
