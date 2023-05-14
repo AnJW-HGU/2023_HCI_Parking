ba0ee81300fda39572918416ba0c7ab377db1f14
AnJW-HGU <22000404@handong.ac.kr>
2023-05-15 08:01:28 +0900

    fix: Edit map screen and streamHDH type for streaming data

--- lib/repository/map_repository.dart
+++ lib/repository/map_repository.dart
@@ -4,15 +4,20 @@ import 'package:hci_parking/model/maps.dart';
 class MapRepository {
   final FirebaseFirestore _firestore = FirebaseFirestore.instance;
 
-  Stream<List<Maps>> getHDH() {
+  Stream<Maps> getHDH() {
     final snapshot = _firestore
         .collection('HDH')
         // .orderBy('closeTime', descending: true)
         .snapshots();
     return snapshot.map((snapshot) {
-      List<Maps> result = [];
+      Maps result = Maps();
       try {
-        result = snapshot.docs.map((e) => Maps.fromJson(e.data())).toList();
+        result = snapshot.docs
+            .map(
+              (e) => Maps.fromJson(e.data()),
+            )
+            .toList()
+            .last;
       } catch (e) {
         rethrow;
       }
