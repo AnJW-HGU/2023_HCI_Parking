defec3123e449a720c65cf43eff7ddf387ee9916
AnJW-HGU <22000404@handong.ac.kr>
2023-05-15 07:03:47 +0900

    feat: Add refresh icon asset and color

--- lib/util/color.dart
+++ lib/util/color.dart
@@ -13,5 +13,9 @@ const appBarColor = Color(0xff3E454D);
 
 const greyColor1 = Color(0xffEEEEEE);
 const greyColor2 = Color(0xff4E4D4D);
+const greyColor3 = Color(0xffC4C9D2);
+
+const dividerColor = Color(0xffEBF3F5);
+const buttonColor = Color(0xff7283A9);
 
 Color shadowColor = const Color(0xff000000).withOpacity(0.25);
